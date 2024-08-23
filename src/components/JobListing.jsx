eb4fb1abcb23dbe94db1b2585ddf7621013c1ae2
karthik <karthik.srinivasprasad@gmail.com>
2024-08-23 19:23:59 +0530

    add react icons package to show icons

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaMapMarker } from "react-icons/fa";
 
 const JobListing = ({
   company,
@@ -38,7 +39,7 @@ const JobListing = ({
 
         <div className="flex flex-col lg:flex-row justify-between mb-4">
           <div className="text-orange-700 mb-3">
-            <i className="fa-solid fa-location-dot text-lg"></i>
+            <FaMapMarker className="inline text-lg mb-1 mr-1" />
             {location}
           </div>
           <a
