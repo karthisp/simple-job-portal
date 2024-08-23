6f4ccc493ad9c2437526a94d8aa04b71222904bd
karthik <karthik.srinivasprasad@gmail.com>
2024-08-23 17:02:53 +0530

    create joblisting component to list all the joblistings

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
       <HomeCard />
 
       {/* <!-- Browse Jobs --> */}
-      <JobListings></JobListings>
+      <JobListings />
 
       <section className="m-auto max-w-lg my-10 px-6">
         <a
